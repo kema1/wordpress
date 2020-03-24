234c07abab1c62c5bb71544f95e26d9de3aaafae
kema1 <3626250@gmail.com>
2020-03-24 16:20:24 +0300

    Наш первый комментарий

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+add_action('wp_enqueue_scripts', 'theme_styles');
+// add_action('wp_footer', 'theme_scripts');
+// add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+// add_action('after_setup_theme', 'theme_register_nav_menu')
+
+// function theme_register_nav_menu() {
+//   register_nav_menu( 'top', 'Меню в шапке' ); 
+// }
+
+function theme_styles() { 
+  wp_enqueue_style('style', get_stylesheet_uri()); 
+  wp_enqueue_style('default', get_template_directory_uri() . '/assets/css/default.css');
+  wp_enqueue_style('layout', get_template_directory_uri() . '/assets/css/layout.css');
+  wp_enqueue_style('media-queries', get_template_directory_uri() . '/assets/css/media-queries.css');
+}
+
+function theme_scripts() {
+  wp_enqueue_script('init', get_template_directory_uri() . '/assets/js/init.js');
+}
+function my_scripts_method(){
+	wp_enqueue_script( 'jquery' );
+}
+?>
